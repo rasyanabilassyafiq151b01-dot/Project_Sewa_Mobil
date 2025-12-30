abbb8d977ece477222bf96a7b18f5ea2c6288a3c
radenmuhammadsulthan-svg <radenmuhammadsulthan@gmail.com>
2025-12-30 18:28:37 +0700

    project1

--- Tampilan.py
+++ Tampilan.py
@@ -1,77 +1,12 @@
-
-print("""
-==============================
-        SELAMAT DATANG
-    DI RENTAL MOBIL BAROKAH
-==============================
-""")
-
-while True:
-
-    print("""
-            ==============================
-                    SELAMAT DATANG
-                DI RENTAL MOBIL BAROKAH
-            ==============================
-
-        SILAHKAN PILIH SEAT YANG ANDA PERLUKAN:
-        
-         1. 4 SEAT    2. 6 SEAT     3. 8 SEAT 
-
-        """)
-
-    p_user = int(input("Masukan Pilihan Anda [1/2/3]:"))
-
-    if p_user == 1:
-        print("""
-    ==============================
-     SILAHKAN PILIH MODEL MOBIL 
-    ==============================
-
-    1. Avanza
-    2. Inova
-    3. Kijang1
-
-        """)
-<<<<<<< HEAD
-        pilihan1 = int(input(("PILIH MODEL MOBIL DIATAS:")))
-        if pilihan1 == 1:
-            
-=======
-        pilihan1 = input("PILIH MODEL MOBIL DIATAS:")
-
-    elif p_user == 2:
-    print("""
-    ==============================
-      SILAHKAN PILIH MODEL MOBIL 
-    ==============================
-
-    1. Avanza
-    2. Inova
-    3. Kijang
-
-        """)
-        pilihan2 = input("PILIH MODEL MOBIL DIATAS:")
-
-    elif p_user == 3:
-    print("""
-    ==============================
-      SILAHKAN PILIH MODEL MOBIL 
-    ==============================
-
-    1. Avanza
-    2. Innova
-    3. Kijang
-
-       """)
-        pilihan3 = input("PILIH MODEL MOBIL DIATAS:")
-
-    else:
-        print("MAAF PILIHAN YANG ANDA PILIH TIDAK ADA!")
-
-<<<<<<< HEAD
-
-# ini aja kontol memwk
-=======
-#fungsi
->>>>>>> 9093da68ded244ac5cac354affc0f91b554e575d
+print("GEROBAK FRIED CHICKEN")
+print("--------------------------")
+print("Jenis Mobil      |   Jenis    Harga")
+print("Toyota Avanza    |    Rp.2500")
+print("Toyota Avanza    |    Rp.2500")
+print("Toyota Avanza    |    Rp.2500")
+print("Toyota Avanza    |    Rp.2500")
+print("Toyota Avanza    |    Rp.2500")
+print("2    Paha    Rp.2000")
+print("3    Sayap   Rp.1500")
+print("4    Keluar  Rp.000")
+print("--------------------------")
