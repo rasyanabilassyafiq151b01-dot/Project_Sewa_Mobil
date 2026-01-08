96835515c7d771b12c7a0ae2f80880b28c62c117
radenmuhammadsulthan-svg <radenmuhammadsulthan@gmail.com>
2026-01-08 14:21:57 +0700

    fix

--- data.py
+++ data.py
@@ -1,6 +1,10 @@
 mobil = {
-    "M1": {"nama": "Avanza", "harga": 350000, "tersedia": True},
-    "M2": {"nama": "Xenia", "harga": 330000, "tersedia": True},
-    "M3": {"nama": "Innova", "harga": 450000, "tersedia": True},
-    "M4": {"nama": "Fortuner", "harga": 650000, "tersedia": True}
+    "M1": {"nama": "Avanza", 
+           "harga": 350000, "tersedia": True},
+    "M2": {"nama": "Xenia", 
+           "harga": 330000, "tersedia": True},
+    "M3": {"nama": "Innova", 
+           "harga": 450000, "tersedia": True},
+    "M4": {"nama": "Fortuner", 
+           "harga": 650000, "tersedia": True}
 }
